9fa678d4092fe8812dd39c4f83ec4ac223a78c13
Timothy Anderson <30357667+Timothy-Anders0n@users.noreply.github.com>
2018-02-20 17:29:00 +0100

    Add files via upload

---
+++ ISM/make_Scutum_comaps.py
@@ -0,0 +1,18 @@
+import sys
+import yaml
+from gascube import gascube
+
+configfile = '../Sgr/Scutum_comaps.yaml'
+
+with open(configfile, "r") as f:
+    config = yaml.load(f)
+
+for s in range(len(config['infile'])):
+    cube = gascube(config['infile'][s])
+    cube.lbmaps(config['lmin'], config['lmax'], #dont use _fit for fast computation
+                config['bmin'], config['bmax'],
+                config['vmin'], config['vmax'],
+                list(config['names'].values()),
+                dcuts = list(config['dcuts'].values()),
+                saveMaps=True,
+                outdir=config['outdir'])
