625e1ae228106a50685e4c7cb0fbde506fb27735
Timothy Anderson <30357667+Timothy-Anders0n@users.noreply.github.com>
2018-02-20 17:25:24 +0100

    Add files via upload

--- Sgr/atlas.py
+++ Sgr/atlas.py
@@ -9,10 +9,11 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 from matplotlib.patches import Circle
 
-comap = fits.open("/Users/ltibaldo/Fermi/ISM/CO/COGAL_deep_mom.fits")[0].data
-himap = fits.open("/Users/ltibaldo/Fermi/ISM/HI/HI4PI/CAR_E03.fits")[0].data
-atlasgal = open("/Users/ltibaldo/Fermi/tangents/images2/table3.dat").readlines()
-bessel = fits.getdata("/Users/ltibaldo/Fermi/tangents/images2/asu.fit", 1)
+comap = fits.open("/home/timothy/Desktop/Fits/COGAL_deep_mom.fits")[0].data
+himap = fits.open("/home/timothy/Desktop/Fits/CAR_E03.fits")[0].data
+E02 = fits.open("/home/timothy/Desktop/Refs/Mine/Data_Set/CAR_E02.fits")[0].data
+bessel = fits.getdata("/home/timothy/Desktop/Fits/asu.fit", 1)
+
 
 ####color coding#########################################
 col=["#4C72B0", "#55A868", "#C44E52","#8172B2", "#CCB974"]
@@ -37,10 +38,10 @@ ax0.scatter([0], [0], facecolor='white')
 ax0.scatter([0], [8.29], facecolor='None', edgecolors='black')
 ax0.plot([-15, 15], [8.29, 8.29], color='black', linewidth=0.5)
 ax0.plot([0, 0], [-18, 18], color='black', linewidth=0.5)
-ax0.text(16, 7.9, "$l=90^\circ$", usetex=True)
-ax0.text(-19.5, 7.9, "$l=-90^\circ$", usetex=True)
-ax0.text(-1.25, -19, "$l=0^\circ$", usetex=True)
-ax0.text(-1.45, 18.5, "$l=180^\circ$", usetex=True)
+#ax0.text(16, 7.9, "$l=90^\circ$", usetex=False)
+#ax0.text(-19.5, 7.9, "$l=-90^\circ$", usetex=False)
+#ax0.text(-1.25, -19, "$l=0^\circ$", usetex=False)
+#ax0.text(-1.45, 18.5, "$l=180^\circ$", usetex=False)
 ax0.set_xlim(-20.,20.)
 ax0.set_ylim(-20.,20.)
 
@@ -53,8 +54,8 @@ for s in range(Narm):
 fig1 = plt.figure("lonvel", figsize=(12, 4))
 fig1.subplots_adjust(left=0.08, right=0.97, top=0.99, bottom=0.14)
 ax1 = plt.subplot(111)
-ax1.set_xlabel("Galactic longitude [$^\circ$]", fontsize=14, usetex=True)
-ax1.set_ylabel("velocity [km s$^{-1}$]", fontsize=14, usetex=True)
+#ax1.set_xlabel("Galactic longitude [$^\circ$]", fontsize=14, usetex=True)
+#ax1.set_ylabel("velocity [km s$^{-1}$]", fontsize=14, usetex=True)
 for tick in ax1.xaxis.get_major_ticks():
     tick.label.set_fontsize(14)
 for tick in ax1.yaxis.get_major_ticks():
@@ -74,8 +75,8 @@ ax1.xaxis.set_ticks(np.arange(180, -210, -30))
 fig2 = plt.figure("lonvelhi", figsize=(6, 5))
 fig2.subplots_adjust(left=0.16, right=0.97, top=0.99, bottom=0.14)
 ax2 = plt.subplot(111)
-ax2.set_xlabel("Galactic longitude [$^\circ$]", fontsize=14, usetex=True)
-ax2.set_ylabel("velocity [km s$^{-1}$]", fontsize=14, usetex=True)
+#ax2.set_xlabel("Galactic longitude [$^\circ$]", fontsize=14, usetex=True)
+#ax2.set_ylabel("velocity [km s$^{-1}$]", fontsize=14, usetex=True)
 for tick in ax2.xaxis.get_major_ticks():
     tick.label.set_fontsize(14)
 for tick in ax2.yaxis.get_major_ticks():
@@ -83,10 +84,20 @@ for tick in ax2.yaxis.get_major_ticks():
 ax2.xaxis.set_ticks_position('both')
 ax2.yaxis.set_ticks_position('both')
 
-himap[np.isnan(himap) == True] = 0.
-himap = np.sum(himap[:,72:193,:], axis=1)
-himap[himap < 0.1] = 0.1
-hi_plot = ax2.imshow(np.sqrt(himap[347:585, :]), cmap="gray_r", origin='lower', extent=[61.083, 39., -153.17, 153.17])
+
+#himap
+#himap[np.isnan(himap) == True] = 0.
+#himap = np.sum(himap[:,72:193,:], axis=1)
+#himap[himap < 0.1] = 0.1
+#hi_plot = ax2.imshow(np.sqrt(himap[347:585, :]), cmap="gray_r", origin='lower', extent=[61.083, 39., -153.17, 153.17])
+#ax2.set_aspect("auto", adjustable="box")
+
+# Val = CRVAL + (PIX - CRPIX)*CDELT
+#E02
+E02[np.isnan(E02) == True] = 0. #Fortan pixel commence a 1, Python pixel commence a 0
+E02 = np.sum(E02[:,72:193,:], axis=1) # Integrale en latitude entre -5 +5 deg
+E02[E02 < 0.1] = 0.1 # supprimer du bruit
+hi_plot = ax2.imshow(np.sqrt(E02[347:585, :]), cmap="gray_r", origin='lower', extent=[41.08, 19., -153.17, 153.17]) # plot montre valeur image longitude vitesse avec limitation en vitesse
 ax2.set_aspect("auto", adjustable="box")
 
 # overlay spiral arm model
@@ -116,30 +127,6 @@ for s in range(Narm):
         ax2.plot(ll, vlsr, color=col[s], linewidth=10, label=lab[s], alpha=0.5)
 ax1.legend()
 
-#overlay ATLASGAL clumps
-for clump in atlasgal:
-    clump = clump.split(' ')
-    clump = [entry for entry in clump if entry != '']
-    try:
-        M = float(clump[11])
-        if M>=3:
-            try:
-                vlsr = float(clump[6])
-                lon = float(clump[0].split("+")[0].split("-")[0].split("L")[1])
-                R = float(clump[8])
-                if lon>180.:
-                    lon -=360
-                ax1.plot(lon,vlsr,marker='o',color='r',alpha=0.3,markersize=M-2)
-                ax2.plot(lon, vlsr, marker='o', color='r', alpha=0.3, markersize=M - 2)
-            except:
-                pass
-                #print(clump[0],"velocity unknown")
-        else:
-            pass
-    except:
-        pass
-        #print(clump[0],"Mass unknown")
-
 #overlay BeSSel high-mass SFR
 for obj in bessel:
     ra = obj['RAJ2000']
@@ -148,7 +135,7 @@ for obj in bessel:
     c = SkyCoord(ra=ra * u.degree, dec=dec * u.degree, frame='icrs')
     l = c.galactic.l.deg
     b = c.galactic.b.deg
-    dist = 1./obj['plx']#distance from the Earth in kpc
+    dist = 1./obj['plx'] #distance from the Earth in kpc
     if l>180:
         l -=360.
     try:
@@ -157,21 +144,44 @@ for obj in bessel:
     except:
         ax1.plot(l, vlsr, marker='^', color='k', alpha=0.3)
         ax2.plot(l, vlsr, marker='^', color='k', alpha=0.3)
-    x, y = lbd2xy(l,b,dist)
+    x, y = lbd2xy(l,b,dist) # Method LBD to XY L lOngi B lat D Distance = 1/parallax
     try:
-        ax0.plot(x, y, marker='^', color=coldic[obj["Arm"]], alpha=0.7)
+        ax0.plot(x, y, marker='^', color=coldic[obj["Arm"]], alpha=0.7) # ignore that
     except:
-        ax0.plot(x, y, marker='^', color='k', alpha=0.3)
-    if obj['Arm'] == 'Sgr' and l > 46.:
+        ax0.plot(x, y, marker='^', color='k', alpha=0.3) # Line to plot after getting lbd
+    if obj['Arm'] == 'Sgr' and l > 46.: # To ignore
         print('SFR')
         print(l,vlsr,dist)
 
+# c = SkyCoord('00 42 30 +41 12 00', unit=(u.hourangle, u.deg))
+
 #add boundaries for study of Sgr tangent
-lmin = 46.
-lmax = 54.
-bounds=[1.5,3.,7.,9]
+#lmin = 46.
+#lmax = 54.
+#bounds=[1.5,3.,7.,9]
+#boundcolors = ['k','g','b','r']
+#x,y = lbd2xy(lmin,0,R0) #Tangents
+#y2 = R0 + 2*x*(y-R0)/x
+#ax0.plot([0,2*x],[R0,y2],linestyle='--',color='k')
+#x,y = lbd2xy(lmax,0,R0)
+#y2 = R0 + 2*x*(y-R0)/x
+#ax0.plot([0,2*x],[R0,y2],linestyle='--',color='k')
+#ax1.vlines([lmin,lmax],-157.3, 157.3,linestyle='--',color='k')
+#ax2.vlines([lmin,lmax],-153.17, 153.17,linestyle='--',color='k')
+#lvals=np.linspace(lmin,lmax,200) #Circles
+#for s, bound in enumerate(bounds):
+#    circ = Circle((0,R0),bound,color=boundcolors[s], linewidth=1.5, linestyle='--',fill=False,zorder=3)
+#    ax0.add_patch(circ)
+#    vbound = lbd2vlsr(lvals,0,bound)
+#    ax1.plot(lvals,vbound,color=boundcolors[s], linewidth=1.5, linestyle='--')
+#    ax2.plot(lvals, vbound, color=boundcolors[s], linewidth=1.5, linestyle='--')
+
+#add boundaries for the study of Scutum tangent
+lmin = 27.
+lmax = 34.5
+bounds=[1.2,4.8,9.5,13.2]
 boundcolors = ['k','g','b','r']
-x,y = lbd2xy(lmin,0,R0)
+x,y = lbd2xy(lmin,0,R0) #Tangents
 y2 = R0 + 2*x*(y-R0)/x
 ax0.plot([0,2*x],[R0,y2],linestyle='--',color='k')
 x,y = lbd2xy(lmax,0,R0)
@@ -179,7 +189,7 @@ y2 = R0 + 2*x*(y-R0)/x
 ax0.plot([0,2*x],[R0,y2],linestyle='--',color='k')
 ax1.vlines([lmin,lmax],-157.3, 157.3,linestyle='--',color='k')
 ax2.vlines([lmin,lmax],-153.17, 153.17,linestyle='--',color='k')
-lvals=np.linspace(lmin,lmax,200)
+lvals=np.linspace(lmin,lmax,200) #Circles
 for s, bound in enumerate(bounds):
     circ = Circle((0,R0),bound,color=boundcolors[s], linewidth=1.5, linestyle='--',fill=False,zorder=3)
     ax0.add_patch(circ)
