215a7c94a25181c764c7f6704cd5039562d821fd
L. Tibaldo <ltibaldo@slac.stanford.edu>
2018-03-09 10:48:44 +0100

    include spin temperature

--- ISM/make_himaps.py
+++ ISM/make_himaps.py
@@ -7,8 +7,13 @@ configfile = sys.argv[1]
 with open(configfile, "r") as f:
     config = yaml.load(f)
 
+try:
+    Ts = config['Ts']
+except:
+    Ts = -10
+
 for s in range(len(config['infile'])):
-    cube = gascube(config['infile'][s], int2col=1.823e-2,
+    cube = gascube(config['infile'][s], int2col=1.823e-2, Ts=Ts,
                    fitres_files=[config['fitres'][s],
                                  config['fitdiag'][s]])
     cube.lbmaps(config['lmin'], config['lmax'],
