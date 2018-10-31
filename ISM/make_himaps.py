13bb268d39a15125d32af0bc7bec7d84251ae1e0
Luigi Tibaldo <luigi.tibaldo@irap.omp.eu>
2018-10-31 10:42:15 +0100

    working on hires data

--- ISM/make_himaps.py
+++ ISM/make_himaps.py
@@ -12,14 +12,22 @@ try:
 except:
     Ts = -10
 
+try:
+    name_tag = config['name_tag']
+except:
+    name_tag = ''
+
 for s in range(len(config['infile'])):
+    try:
+        fitres_files=[config['fitres'][s],config['fitdiag'][s]]
+    except:
+        fitres_files=[None,None]
     cube = gascube(config['infile'][s], int2col=1.823e-2, Ts=Ts,
-                   fitres_files=[config['fitres'][s],
-                                 config['fitdiag'][s]])
+                   fitres_files=fitres_files)
     cube.lbmaps(config['lmin'], config['lmax'],
                 config['bmin'], config['bmax'],
                 config['vmin'], config['vmax'],
                 list(config['names'].values()),
                 dcuts=list(config['dcuts'].values()),
                 saveMaps=True, useFit=config['useFit'],
-                outdir=config['outdir'])
+                outdir=config['outdir'], name_tag = name_tag)
